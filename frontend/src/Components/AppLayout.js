d1ae4cbebf55b2056343b5c4fb4b81ad73e38ee9
root <gogia.prateek@hotmail.com>
2018-07-09 00:36:58 +0530

    Added a new component for image carousel

--- frontend/src/Components/AppLayout.js
+++ frontend/src/Components/AppLayout.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import Header from '../Components/Header';
+import React, { Component } from "react";
+import Header from "../Components/Header";
+import ImageSlider from "./ImageSlider";
 
 export default class AppLayout extends Component {
   render() {
     return (
       <div>
         <Header />
+        <ImageSlider />
       </div>
-    )
+    );
   }
 }
