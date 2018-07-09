5e12debd326466cd4dc3d5e83d5b192f7f5cd1d6
root <gogia.prateek@hotmail.com>
2018-07-10 00:49:34 +0530

    Added Introduction about the author

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -9,7 +9,8 @@ import {
   CardLink,
   CardTitle,
   CardSubtitle,
-  CardText
+  CardText,
+  CardImg
 } from "reactstrap";
 
 class Home extends Component {
@@ -76,8 +77,24 @@ class Home extends Component {
             </Col>
             <Col sm="4">
               <Card>
-                <CardTitle className="text-center">Hi, I am Sorabh</CardTitle>
-                <CardSubtitle />
+                <div className="about-image">
+                  <CardImg
+                    src="https://scontent.ffjr1-4.fna.fbcdn.net/v/t1.0-9/31490594_10216669808566432_3735181059290174503_n.jpg?_nc_fx=ffjr1-1&_nc_cat=0&oh=69a9146ff5da92101ca90a2b5ca225a0&oe=5BE335E6"
+                    top
+                    width="50"
+                    height="50"
+                  />
+                </div>
+                <CardTitle className="text-center author-title">
+                  Hi, I am Sorabh!
+                </CardTitle>
+                <CardBody>
+                  <CardText className="description">
+                    My name is Sorabh, Etiam volutpat leo nec libero feugiat, vel
+                    malesuada neque commodo. Curabitur sem nibh, auctor a
+                    viverra ut, dictum ullamcorper libero.
+                  </CardText>
+                </CardBody>
               </Card>
             </Col>
           </Row>
