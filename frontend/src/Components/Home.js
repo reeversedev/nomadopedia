7c90da052bd3f63265893d48f4f831bbacde9e14
root <gogia.prateek@hotmail.com>
2018-07-10 00:26:14 +0530

    Added the content related to post

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -37,6 +37,40 @@ class Home extends Component {
                     src="https://themes.wplook.com/morning-time/wp-content/uploads/sites/16/2018/03/caleb-george-144977-unsplash-1280x800.jpg"
                     width="100%"
                   />
+                  <CardBody>
+                    <CardText>
+                      Bearing cattle void spirit third whose fill let shall our
+                      also signs. Fly subdue of herb won’t. Lights one Man whose
+                      green two. Hath, void created.
+                    </CardText>
+                    <CardText>
+                      In over under beast seed He. Thing Second stars green
+                      there gathering living. Without greater image moving
+                      living signs fruit above heaven. Hath, behold isn’t Day.
+                    </CardText>
+                    <CardText>
+                      Isn’t saw fill green open brought grass lights darkness
+                      doesn’t seas, stars tree void fowl god Replenish had
+                      third, make lights likeness years above night, creeping is
+                      divide let first may after. Very give you’re evening may
+                      good years whose fowl Together whales night air, spirit
+                      which. Whose creepeth can’t dry second and whales. Their
+                      herb winged.
+                    </CardText>
+                    <CardText>
+                      Kind fowl beginning, every man saying, so fish without
+                      replenish made won’t good, face i. Beginning. Which good
+                      Moving in without given won’t hath fish said, sea beast
+                      herb a waters to form creepeth our man open shall subdue.
+                      Him whose.
+                    </CardText>
+                    <CardText>
+                      Life so moving unto lesser fourth very seed and without
+                      may whose Replenish were seas life heaven place were.
+                      Seasons earth of great creepeth Unto. Make to herb.
+                      Behold.
+                    </CardText>
+                  </CardBody>
                 </Card>
               </div>
             </Col>
