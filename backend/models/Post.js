5d8917e2fc6415830049f706644e2e323451a317
root <gogia.prateek@hotmail.com>
2018-07-19 12:10:29 +0530

    Made Post Schema

---
+++ backend/models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  slug: String,
+  date: String,
+  tags: {
+    tag1: String,
+    tag2: String,
+    tag3: String
+  },
+  content: Object,
+  author: String,
+  type: String,
+  photosUrl: Object,
+  subcategory: String
+});
+
+const Post = (module.exports = mongoose.model("Post", postSchema));
